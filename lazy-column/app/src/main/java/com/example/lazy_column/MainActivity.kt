9a0642c44648f417842734aa19ecb0b280041766
derek-alves <derek.alves1998@hotmail.com>
2022-07-13 20:15:01 -0300

    first lazzy column

---
+++ lazy-column/app/src/main/java/com/example/lazy_column/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.lazy_column
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.lazy_column.ui.theme.LazycolumnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumnDemo()
+        }
+    }
+}
+
+@Composable
+fun ScrollableColumnDemo() {
+    val scrollState = rememberScrollState()
+    Column(modifier = Modifier.verticalScroll(scrollState)){
+        for(i in 1..100){
+            Text(
+                "User name $i",
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(10.dp)
+            )
+            Divider(color= Color.Black, thickness = 5.dp)
+        }
+    }
+}
+
+
+@Composable
+fun LazyColumnDemo(){
+    LazyColumn{
+        items(count = 100) {
+            Text(
+                "User name $it",
+                style = MaterialTheme.typography.h3,
+                modifier = Modifier.padding(10.dp)
+            )
+            Divider(color= Color.Black, thickness = 5.dp)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ScrollableColumnDemo()
+}
