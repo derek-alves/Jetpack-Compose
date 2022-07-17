960105549baa8f198bd0e23289aefbabfeb3e871
derek-alves <derek.alves1998@hotmail.com>
2022-07-17 12:33:19 -0300

    step: change organization of file

--- lazy-column/app/src/main/java/com/example/lazy_column/MainActivity.kt
+++ lazy-column/app/src/main/java/com/example/lazy_column/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
@@ -17,7 +16,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.lazy_column.ui.theme.LazycolumnTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
