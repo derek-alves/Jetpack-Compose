d9cd77844cc836149333f8434a63465b479f3a35
derek-alves <derek.alves1998@hotmail.com>
2022-07-19 01:44:46 -0300

    step: add navigation to screen detailss

--- lazy-column/app/src/main/java/com/example/lazy_column/MainActivity.kt
+++ lazy-column/app/src/main/java/com/example/lazy_column/MainActivity.kt
@@ -6,16 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.lazy_column.compose.TvShowListItem
 import com.example.lazy_column.data.TvShowList
 import com.example.lazy_column.model.TvShow
 
@@ -23,8 +27,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LazyColumnDemo2{
-                Toast.makeText(this,it,Toast.LENGTH_SHORT).show()
+            DisplayTvShows {
+                startActivity(InfoTvShowActivity.intent(this,it))
             }
         }
     }
@@ -33,40 +37,51 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DisplayTvShows(selectedItem: (TvShow) -> Unit) {
-    val tvShows = TvShowList.tvShows
+    val tvShows = remember { TvShowList.tvShows }
+
+    LazyColumn(contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+        items(
+            items = tvShows,
+            itemContent = {
+                TvShowListItem(tvShow = it, selectedItem = selectedItem)
+            }
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    LazyColumnDemo()
+    DisplayTvShows {}
 }
 
 @Composable
-fun LazyColumnDemo2(selectedItem: (String)->(Unit)){
-    LazyColumn{
+fun LazyColumnDemo2(selectedItem: (String) -> (Unit)) {
+    LazyColumn {
         items(count = 100) {
             Text(
                 "User name $it",
                 style = MaterialTheme.typography.h3,
-                modifier = Modifier.padding(10.dp)
+                modifier = Modifier
+                    .padding(10.dp)
                     .clickable { selectedItem("$it Selected") }
             )
-            Divider(color= Color.Black, thickness = 5.dp)
+            Divider(color = Color.Black, thickness = 5.dp)
         }
     }
 }
 
 @Composable
-fun LazyColumnDemo(){
-    LazyColumn{
+fun LazyColumnDemo() {
+    LazyColumn {
         items(count = 100) {
             Text(
                 "User name $it",
                 style = MaterialTheme.typography.h3,
                 modifier = Modifier.padding(10.dp)
             )
-            Divider(color= Color.Black, thickness = 5.dp)
+            Divider(color = Color.Black, thickness = 5.dp)
         }
     }
 }
@@ -74,14 +89,14 @@ fun LazyColumnDemo(){
 @Composable
 fun ScrollableColumnDemo() {
     val scrollState = rememberScrollState()
-    Column(modifier = Modifier.verticalScroll(scrollState)){
-        for(i in 1..100){
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
+        for (i in 1..100) {
             Text(
                 "User name $i",
                 style = MaterialTheme.typography.h3,
                 modifier = Modifier.padding(10.dp)
             )
-            Divider(color= Color.Black, thickness = 5.dp)
+            Divider(color = Color.Black, thickness = 5.dp)
         }
     }
 }
